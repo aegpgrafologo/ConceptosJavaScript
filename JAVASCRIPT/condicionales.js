112309a9ea2169e03a169bc998de83bcd61725e2
aegpgrafologo <anyelo290681@gmail.com>
2025-02-11 22:25:43 -0500

    Tercera Actualización

---
+++ JAVASCRIPT/condicionales.js
@@ -0,0 +1,26 @@
+'use strict'
+//Condicionales - Instrucciones que permite comparar algo.
+/*var edad1 = 12;
+var edad2 = 50;
+//Si pasa esto
+if(edad1>edad2){
+//ejecuta esto.
+    console.log("Edad1 es mayor que edad2");
+}else{
+    console.log("Edad1 es inferior que edad2");  
+}*/
+/*Operadores Relacionales
+Mayor >
+Menor <
+Mayor o igual >=
+Menor o igual <=
+Igual ==
+Diferente !=
+*/
+var edad = 16;
+var nombre = "Anyelo Gomez";
+if(edad>=18){
+    //Usuario es mayor de edad
+    console.log(nombre+ " tiene "+edad+ " años, es mayor de edad ");
+
+}
